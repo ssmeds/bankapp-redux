827b4ca5722ffb09eca734afff30fa5cf4b7ec88
ssmeds <stina_smeds@hotmail.com>
2022-09-21 15:07:53 +0200

    removed some comments

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,28 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import reducers from "./reducers";
-// import thunk from 'redux-thunk';
-import bankReducer from './reducers'
 
-// const reducer = {
-//   bank: bankReducer,
- 
-// }
-// const preloadedState = {
-//   todos: [
-//     {
-//       text: 'Eat food',
-//       completed: true,
-//     },
-//     {
-//       text: 'Exercise',
-//       completed: false,
-//     },
-//   ],
-//   visibilityFilter: 'SHOW_COMPLETED',
-// }
+import bankReducer from './reducers'
 
 export const store = configureStore({
   reducer: bankReducer,
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-  // preloadedState,
 })
